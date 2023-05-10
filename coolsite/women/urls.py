39e24017dcca381552e2089f810be7ecd5392e23
Krasnokutskyi Valentyn <kras.vvk@gmail.com>
2023-05-10 14:00:13 -0400

    Lesson #6

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -1,8 +1,7 @@
-from django.urls import path, re_path
+from django.urls import path
 from .views import *
 
 urlpatterns = [
     path('', index, name='home'),
-    path('cats/<int:catid>/', categories),
-    re_path(r'archive/(?P<year>[0-9]{4})/', archive),
+    path('about/', about, name='about'),
 ]
