56edd50944129cac01a5a3ca1ebd8a17cf32bc0d
Krasnokutskyi Valentyn <kras.vvk@gmail.com>
2023-05-10 19:00:01 -0400

    Lesson #7

--- coolsite/women/models.py
+++ coolsite/women/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-
 class Women(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
-    photo = models.ImageField(upload_to="photos/%Y/%m/%d")
-    time_creat = models.DateTimeField(auto_now_add=True)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
+    time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
-    if_published = models.BooleanField(default=True)
+    is_published = models.BooleanField(default=True)
 
     def __str__(self):
         return self.title
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -3,27 +3,27 @@ from django.shortcuts import render, redirect
 
 from .models import *
 
-menu = ["О сайте", " Добавить статью", "Обратная связь", "Войти"]
-
+menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 
 def index(request):
     posts = Women.objects.all()
     return render(request, 'women/index.html', {'posts': posts, 'menu': menu, 'title': 'Главная страница'})
-
 
 def about(request):
     return render(request, 'women/about.html', {'menu': menu, 'title': 'О сайте'})
 
 
 def categories(request, catid):
-    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
+    if request.POST:
+        print(request.POST)
 
+    return HttpResponse(f"<h1>Статьи по категориям</h1><p>{catid}</p>")
 
 def archive(request, year):
     if int(year) > 2020:
-        return redirect('home', permanent=True)
+        return redirect('home', permanent=False)
+
     return HttpResponse(f"<h1>Архив по годам</h1><p>{year}</p>")
 
-
 def pageNotFound(request, exception):
-    return HttpResponseNotFound(f"<h1>Страница не найдена</h1>")
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
