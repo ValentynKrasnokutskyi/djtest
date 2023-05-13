3de886d87c8b0d7f729218ba17e7dae70302e12f
Krasnokutskyi Valentyn <kras.vvk@gmail.com>
2023-05-12 23:49:57 -0400

    Lesson #12

--- coolsite/women/templatetags/women_tags.py
+++ coolsite/women/templatetags/women_tags.py
@@ -1,9 +1,7 @@
 from django import template
 from women.models import *
 
-
 register = template.Library()
-
 
 @register.simple_tag(name='getcats')
 def get_categories(filter=None):
@@ -12,11 +10,11 @@ def get_categories(filter=None):
     else:
         return Category.objects.filter(pk=filter)
 
-
 @register.inclusion_tag('women/list_categories.html')
 def show_categories(sort=None, cat_selected=0):
     if not sort:
         cats = Category.objects.all()
     else:
         cats = Category.objects.order_by(sort)
-    return {'cats': cats, 'cat_selected': cat_selected}
+
+    return {"cats": cats, "cat_selected": cat_selected}
