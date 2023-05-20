7c3343aafba20f0b6bef8acd28a38e8edaea3f4a
Krasnokutskyi Valentyn <kras.vvk@gmail.com>
2023-05-20 17:58:01 -0400

    Lesson #20

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -124,3 +124,5 @@ STATICFILES_DIRS = []
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+#LOGIN_REDIRECT_URL = '/'
